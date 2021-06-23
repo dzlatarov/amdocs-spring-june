346ef5aa1a73f6377d46019602bcbb46b663aa6c
User <User@Lab-637594450039872288-1>
2021-06-24 00:17:35 +0530

    project initial commit

---
+++ MVCex/src/main/java/com/amdocs/mvcex/MvCexApplication.java
@@ -0,0 +1,13 @@
+package com.amdocs.mvcex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvCexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvCexApplication.class, args);
+	}
+
+}
